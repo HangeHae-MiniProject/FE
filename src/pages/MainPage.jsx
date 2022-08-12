87aac54fdad767218f6f11f607bbc2442723080f
jhhyung <jinhahyung@gmail.com>
2022-08-12 23:01:44 +0900

    router기본설정을 완료했습니다.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "../css_modules/Main.module.css";
 
 function MainPage() {
   const nav = useNavigate();
   return (
     <div>
-      <div>
+      <div className={styles.MainColor}>
         <button></button>
       </div>
     </div>

bc6d630883198952608b8b16beb7236fd5b15a95
jhhyung <jinhahyung@gmail.com>
2022-08-12 22:05:52 +0900

    메인페이지 코드뼈대작업커밋

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function MainPage() {
+  const nav = useNavigate();
+  return (
+    <div>
+      <div>
+        <button></button>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;

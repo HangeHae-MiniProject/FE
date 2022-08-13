d8253c2d514b0c6078e66a36b582f4a34e02ddc8
jhhyung <jinhahyung@gmail.com>
2022-08-13 20:28:09 +0900

    뼈대 그린 내용

--- src/components/component/Result.jsx
+++ src/components/component/Result.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-import styles from "../../css_modules/Main.module.css";
 import { useNavigate } from "react-router-dom";
+
+import styles from "../../css_modules/ResultPage.module.css";
 import Btn from "../elements/Btn";
 
 function ResultPage() {
   const nav = useNavigate();
   return (
-    <div>
-      <div className={styles.MainColor}>
-        <h1>당신에게 추천하는 나라는...</h1>
-        <img
-          src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/d6/96/36/photo4jpg.jpg?w=700&h=-1&s=1"
-          alt="국가이미지"
-        />
-        <h2>네팔입니다</h2>
-        <p>네팔은 코로나19관련 격리면제가 가능한 나라입니다.</p>
-        <p>더 자세한 내용은 아래를 통해 확인해보세요.</p>
-        <Btn onClick={() => nav("/login")}>결과 저장</Btn>
-      </div>
+    <div className={styles.ResultWrap}>
+      <h1>당신에게 추천하는 나라는</h1>
+      <img
+        src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/d6/96/36/photo4jpg.jpg?w=700&h=-1&s=1"
+        alt="국가이미지"
+      />
+      <h2>네팔입니다</h2>
+      <p>네팔은 코로나19관련 격리면제가 가능한 나라입니다.</p>
+      <p>더 자세한 내용은 아래를 통해 확인해보세요.</p>
+      <Btn onClick={() => nav("/login")}>결과 저장</Btn>
     </div>
   );
 }

5e79e84dfc2c0118b778cfdd5f4a8c542d86569f
jhhyung <jinhahyung@gmail.com>
2022-08-13 11:32:07 +0900

    router주소 result->results로 변경

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -13,7 +13,7 @@ const Router = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/join" element={<JoinPage />} />
       <Route path="/question" element={<QuePage />} />
-      <Route path="/result" element={<ResultPage />} />
+      <Route path="/results" element={<ResultPage />} />
     </Routes>
   );
 };

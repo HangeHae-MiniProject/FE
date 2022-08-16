e62d2330cd4f8ccdd7958dd478b507d99d951657
JJOOYYONG <https://github.com/JJOOYYONG>
2022-08-16 09:27:33 +0900

    헤더에서 홈으로 이동 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import styles from "../../css_modules/Header.module.css"
+import styles from "../../css_modules/Header.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const nav = useNavigate();
   return (
     <div className={styles.title}>
-      <h1>TRAPICK</h1>
+      <h1 onClick={() => nav("/")}>TRAPICK</h1>
     </div>
-  )
+  );
 };
 
 export default Header;

d2f17f9905164fda04e952ba3ecb7e6053113454
jhhyung <jinhahyung@gmail.com>
2022-08-16 22:59:39 +0900

    메인페이지에 로그인버튼 추가

--- src/components/component/Main.jsx
+++ src/components/component/Main.jsx
@@ -11,9 +11,18 @@ function Main() {
       <div className={styles.MainTitle}>
         당신을 위한
         <h1>추천 여행지는?</h1>
+        <div onClick={() => nav("/login")} className={styles.userCheck}>
+          이미 회원이신가요?
+        </div>
       </div>
+
       <div className={styles.btnContainer}>
-        <Btn onClick={() => nav("/question")} backgroundColor="black">
+        <Btn
+          onClick={() => nav("/question")}
+          backgroundColor="black"
+          width="220px"
+          height="3rem"
+        >
           테스트하러 가기
         </Btn>
       </div>
